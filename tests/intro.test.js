4a8cbdd4dd9b6960fd9e9856ac5bd30eff0f912a
holly bishop <hollybish10@hotmail.co.uk>
2025-08-07 09:36:38 +0100

    2.6 - Writing Your First Test

---
+++ tests/intro.test.js
@@ -0,0 +1,30 @@
+// test and it are aliases for the same function
+import { describe, test, it, expect } from "vitest";
+import { max } from "../src/intro";
+
+
+describe('max', ()=>{
+    test('should return the first argument if it is greater', ()=>{
+        // // Arrange: Turn on the TV
+        // const a = 2;
+        // const b = 1;
+
+        // // Act - Press the power button
+        // const result = max(a, b);
+
+        // Assert - Check the TV is off
+        // expect(result).toBe(2);
+
+        // Combined all of the above
+        expect(max(2,1)).toBe(2);
+    })
+
+        test('should return the second argument if it is greater', ()=>{
+        expect(max(1,2)).toBe(2);
+    })
+ 
+    test('should return the first argument if arguments are equal', ()=>{
+        expect(max(2,2)).toBe(2);
+    })
+    
+})
