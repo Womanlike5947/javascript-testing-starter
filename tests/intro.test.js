6a4ccf539bbae3a5eea2b3aa4dfa7af05b0e2cff
holly bishop <hollybish10@hotmail.co.uk>
2025-08-07 10:41:35 +0100

    2.7 - Testing fizzBuzz and adding Prettier formatting

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,30 +1,42 @@
 // test and it are aliases for the same function
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
+describe("max", () => {
+  test("should return the first argument if it is greater", () => {
+    // // Arrange: Turn on the TV
+    // const a = 2;
+    // const b = 1;
 
-describe('max', ()=>{
-    test('should return the first argument if it is greater', ()=>{
-        // // Arrange: Turn on the TV
-        // const a = 2;
-        // const b = 1;
+    // // Act - Press the power button
+    // const result = max(a, b);
 
-        // // Act - Press the power button
-        // const result = max(a, b);
+    // Assert - Check the TV is off
+    // expect(result).toBe(2);
 
-        // Assert - Check the TV is off
-        // expect(result).toBe(2);
+    // Combined all of the above
+    expect(max(2, 1)).toBe(2);
+  });
 
-        // Combined all of the above
-        expect(max(2,1)).toBe(2);
-    })
+  test("should return the second argument if it is greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
 
-        test('should return the second argument if it is greater', ()=>{
-        expect(max(1,2)).toBe(2);
-    })
- 
-    test('should return the first argument if arguments are equal', ()=>{
-        expect(max(2,2)).toBe(2);
-    })
-    
-})
+  test("should return the first argument if arguments are equal", () => {
+    expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizzBuzz", () => {
+  test('should return "fizzBuzz" when number is divisible by 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  test('should return "Fizz" when number is divisible by 3', () => {
+    expect(fizzBuzz(9)).toBe("Fizz");
+  });
+
+  test('should return "Fizz" when number is divisible by 5', () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+});
