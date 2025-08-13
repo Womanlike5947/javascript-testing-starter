2a1325e7aba1e177070ef090d990ecce159e986f
holly bishop <hollybish10@hotmail.co.uk>
2025-08-13 14:21:22 +0100

    5 - Catching Type Errors with TypeScript

---
+++ src/_tests/main.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "vitest";
+
+import { calculateDiscount } from "../../src/main.js";
+// #region calculateDiscount
+describe("calculateDiscount", () => {
+  test("should return discounted price if given valid code", () => {
+    expect(calculateDiscount(10, "SAVE10")).toBe(9);
+    expect(calculateDiscount(10, "SAVE20")).toBe(8);
+  });
+
+  test("should handle negative price", () => {
+    expect(calculateDiscount(-10, "SAVE10")).toMatch(/invalid/i);
+  });
+
+  test("should handle invalid discount code", () => {
+    expect(calculateDiscount(10, "INVALID")).toBe(10);
+  });
+});
+// #endregion
